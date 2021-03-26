f3556e77600684742c14e26f6832e66a60a2baed
Paulo Rodrigues <pcqrodrigues@gmail.com>
2021-03-26 19:34:42 -0300

    feat: Added repo from mongodb

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,17 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+  async disconnect () {
+    await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
